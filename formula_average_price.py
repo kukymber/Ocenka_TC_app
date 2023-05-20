a24da9f1b45f0bbd86d8b3c1e9e4c508af975d54
kukymber <ostap_to@mail.ru>
2023-05-21 00:30:27 +0800

    added round all results

--- formula_average_price.py
+++ formula_average_price.py
@@ -1,26 +1,20 @@
 import numpy as np
 
 class PriceCalculator:
-    def __init__(self, offer_prices):
-        self.offer_prices = offer_prices
+    def __init__(self):
+        self.offer_prices = []
+
+    def update_offer_prices(self, new_prices):
+        self.offer_prices = new_prices
 
     def compute_average_offer_price(self):
-        return np.mean(self.offer_prices)
+        return round(np.mean(self.offer_prices), 2)
 
     def compute_final_average_offer_price(self):
         average_offer_price = self.compute_average_offer_price()
         final_offers = [price for price in self.offer_prices if abs(price - average_offer_price) / average_offer_price <= 0.2]
-        return np.mean(final_offers)
+        return round(np.mean(final_offers), 2)
 
     def compute_final_price(self):
         final_average_offer_price = self.compute_final_average_offer_price()
-        return final_average_offer_price * 0.9
-
-# Создаем экземпляр класса PriceCalculator и передаем ему цены предложений
-price_calculator = PriceCalculator([396000, 350000, 199000, 255000, 240000])
-
-# Вычисляем итоговую цену
-final_price = price_calculator.compute_final_price()
-
-# Выводим итоговую цену
-print(f"Итоговая цена: {final_price}")
+        return round(final_average_offer_price * 0.9, 2)
